fe74c2ffaf7683b21d58085c26773e799834f01c
cfoytek <cff15@txstate.edu>
2017-04-26 17:28:09 -0500

    initial commit

---
+++ CPUTSP/TSP_CPU.cpp
@@ -0,0 +1,14 @@
+/*
+  Produced for Vikas Chaudary's Computer Architecture Class
+
+  This program is a single threaded random restart hill climbing Travelling
+  Salesman Problem solver with 2-opt local search heuristic. This program
+  is heavily based on TSP_GPU.cu, authored by Molly O'Neil and Martin Burtscher,
+  and is intended to be a fair-comparison-test between multithreaded and single-
+  threaded TSP solvers using this algorithm. For more information about
+  TSP_GPU.cu, visit http://cs.txstate.edu/~burtscher/research/TSP_GPU/.
+*/
+
+int main(int argc, char *argv[]) {
+  
+}
